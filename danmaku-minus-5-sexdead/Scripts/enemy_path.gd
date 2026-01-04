8e061af0944699e8bd0538fce2692a53a1433dd3
SH <77217128+Slon549@users.noreply.github.com>
2026-01-04 15:17:34 +0900

    PathFollow2D 부모의 curve 속성 수정

--- danmaku-minus-5-sexdead/Scripts/enemy_path.gd
+++ danmaku-minus-5-sexdead/Scripts/enemy_path.gd
@@ -1,16 +1,10 @@
 extends PathFollow2D
 
 @export var speed: float = 200.0
-@onready var curve = Curve2D.new()
 
-func _ready():
-    var viewport_size = get_viewport_rect().size
-    
-    # 점들을 절대 값이 아닌 화면 비율로 설정
-    curve.add_point(Vector2(0.0, 0.0) * viewport_size)        # 시작점: 왼쪽 하단
-    curve.add_point(Vector2(0.5, 0.5) * viewport_size)        # 중앙: 중앙 정점
-    curve.add_point(Vector2(1.0, 0.0) * viewport_size)        # 끝점: 오른쪽 하단
-    set_curve(curve)
+func _ready() -> void:
+    if get_parent() is Path2D:
+        get_parent().curve = curve
 
 func _process(delta: float):
     # 매 프레임마다 경로를 따라 progress(픽셀 단위)를 증가시킵니다.
